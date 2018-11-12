cbd25b84276a2403fc8fcf77edc11f22c7fd1efe
SD18 <SD18@mitt.ca>
2018-11-12 15:15:39 -0600

    Initial Commit

---
+++ Calculator/Form1.Designer.vb
@@ -0,0 +1,230 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextB1 = New System.Windows.Forms.TextBox()
+        Me.Number1 = New System.Windows.Forms.Button()
+        Me.Number2 = New System.Windows.Forms.Button()
+        Me.Number3 = New System.Windows.Forms.Button()
+        Me.Number4 = New System.Windows.Forms.Button()
+        Me.Number5 = New System.Windows.Forms.Button()
+        Me.Number6 = New System.Windows.Forms.Button()
+        Me.Number7 = New System.Windows.Forms.Button()
+        Me.Number8 = New System.Windows.Forms.Button()
+        Me.Number9 = New System.Windows.Forms.Button()
+        Me.OperationClear = New System.Windows.Forms.Button()
+        Me.OperationPlus = New System.Windows.Forms.Button()
+        Me.OperationMinus = New System.Windows.Forms.Button()
+        Me.OperationEqual = New System.Windows.Forms.Button()
+        Me.OperationDivide = New System.Windows.Forms.Button()
+        Me.OperationMultiply = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TextB1
+        '
+        Me.TextB1.Location = New System.Drawing.Point(12, 41)
+        Me.TextB1.Name = "TextB1"
+        Me.TextB1.Size = New System.Drawing.Size(435, 22)
+        Me.TextB1.TabIndex = 0
+        Me.TextB1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Number1
+        '
+        Me.Number1.Location = New System.Drawing.Point(12, 92)
+        Me.Number1.Name = "Number1"
+        Me.Number1.Size = New System.Drawing.Size(75, 23)
+        Me.Number1.TabIndex = 1
+        Me.Number1.Text = "1"
+        Me.Number1.UseVisualStyleBackColor = True
+        '
+        'Number2
+        '
+        Me.Number2.Location = New System.Drawing.Point(116, 92)
+        Me.Number2.Name = "Number2"
+        Me.Number2.Size = New System.Drawing.Size(75, 23)
+        Me.Number2.TabIndex = 2
+        Me.Number2.Text = "2"
+        Me.Number2.UseVisualStyleBackColor = True
+        '
+        'Number3
+        '
+        Me.Number3.Location = New System.Drawing.Point(229, 92)
+        Me.Number3.Name = "Number3"
+        Me.Number3.Size = New System.Drawing.Size(75, 23)
+        Me.Number3.TabIndex = 3
+        Me.Number3.Text = "3"
+        Me.Number3.UseVisualStyleBackColor = True
+        '
+        'Number4
+        '
+        Me.Number4.Location = New System.Drawing.Point(12, 157)
+        Me.Number4.Name = "Number4"
+        Me.Number4.Size = New System.Drawing.Size(75, 23)
+        Me.Number4.TabIndex = 4
+        Me.Number4.Text = "4"
+        Me.Number4.UseVisualStyleBackColor = True
+        '
+        'Number5
+        '
+        Me.Number5.Location = New System.Drawing.Point(116, 157)
+        Me.Number5.Name = "Number5"
+        Me.Number5.Size = New System.Drawing.Size(75, 23)
+        Me.Number5.TabIndex = 5
+        Me.Number5.Text = "5"
+        Me.Number5.UseVisualStyleBackColor = True
+        '
+        'Number6
+        '
+        Me.Number6.Location = New System.Drawing.Point(229, 157)
+        Me.Number6.Name = "Number6"
+        Me.Number6.Size = New System.Drawing.Size(75, 23)
+        Me.Number6.TabIndex = 6
+        Me.Number6.Text = "6"
+        Me.Number6.UseVisualStyleBackColor = True
+        '
+        'Number7
+        '
+        Me.Number7.Location = New System.Drawing.Point(12, 217)
+        Me.Number7.Name = "Number7"
+        Me.Number7.Size = New System.Drawing.Size(75, 23)
+        Me.Number7.TabIndex = 7
+        Me.Number7.Text = "7"
+        Me.Number7.UseVisualStyleBackColor = True
+        '
+        'Number8
+        '
+        Me.Number8.Location = New System.Drawing.Point(116, 217)
+        Me.Number8.Name = "Number8"
+        Me.Number8.Size = New System.Drawing.Size(75, 23)
+        Me.Number8.TabIndex = 8
+        Me.Number8.Text = "8"
+        Me.Number8.UseVisualStyleBackColor = True
+        '
+        'Number9
+        '
+        Me.Number9.Location = New System.Drawing.Point(229, 217)
+        Me.Number9.Name = "Number9"
+        Me.Number9.Size = New System.Drawing.Size(75, 23)
+        Me.Number9.TabIndex = 9
+        Me.Number9.Text = "9"
+        Me.Number9.UseVisualStyleBackColor = True
+        '
+        'OperationClear
+        '
+        Me.OperationClear.Location = New System.Drawing.Point(50, 263)
+        Me.OperationClear.Name = "OperationClear"
+        Me.OperationClear.Size = New System.Drawing.Size(331, 42)
+        Me.OperationClear.TabIndex = 10
+        Me.OperationClear.Text = "Clear"
+        Me.OperationClear.UseVisualStyleBackColor = True
+        '
+        'OperationPlus
+        '
+        Me.OperationPlus.Location = New System.Drawing.Point(324, 92)
+        Me.OperationPlus.Name = "OperationPlus"
+        Me.OperationPlus.Size = New System.Drawing.Size(75, 23)
+        Me.OperationPlus.TabIndex = 11
+        Me.OperationPlus.Text = "+"
+        Me.OperationPlus.UseVisualStyleBackColor = True
+        '
+        'OperationMinus
+        '
+        Me.OperationMinus.Location = New System.Drawing.Point(324, 121)
+        Me.OperationMinus.Name = "OperationMinus"
+        Me.OperationMinus.Size = New System.Drawing.Size(75, 23)
+        Me.OperationMinus.TabIndex = 12
+        Me.OperationMinus.Text = "-"
+        Me.OperationMinus.UseVisualStyleBackColor = True
+        '
+        'OperationEqual
+        '
+        Me.OperationEqual.Location = New System.Drawing.Point(324, 217)
+        Me.OperationEqual.Name = "OperationEqual"
+        Me.OperationEqual.Size = New System.Drawing.Size(75, 23)
+        Me.OperationEqual.TabIndex = 13
+        Me.OperationEqual.Text = "="
+        Me.OperationEqual.UseVisualStyleBackColor = True
+        '
+        'OperationDivide
+        '
+        Me.OperationDivide.Location = New System.Drawing.Point(324, 186)
+        Me.OperationDivide.Name = "OperationDivide"
+        Me.OperationDivide.Size = New System.Drawing.Size(75, 23)
+        Me.OperationDivide.TabIndex = 14
+        Me.OperationDivide.Text = "/"
+        Me.OperationDivide.UseVisualStyleBackColor = True
+        '
+        'OperationMultiply
+        '
+        Me.OperationMultiply.Location = New System.Drawing.Point(324, 157)
+        Me.OperationMultiply.Name = "OperationMultiply"
+        Me.OperationMultiply.Size = New System.Drawing.Size(75, 23)
+        Me.OperationMultiply.TabIndex = 15
+        Me.OperationMultiply.Text = "*"
+        Me.OperationMultiply.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(460, 450)
+        Me.Controls.Add(Me.OperationMultiply)
+        Me.Controls.Add(Me.OperationDivide)
+        Me.Controls.Add(Me.OperationEqual)
+        Me.Controls.Add(Me.OperationMinus)
+        Me.Controls.Add(Me.OperationPlus)
+        Me.Controls.Add(Me.OperationClear)
+        Me.Controls.Add(Me.Number9)
+        Me.Controls.Add(Me.Number8)
+        Me.Controls.Add(Me.Number7)
+        Me.Controls.Add(Me.Number6)
+        Me.Controls.Add(Me.Number5)
+        Me.Controls.Add(Me.Number4)
+        Me.Controls.Add(Me.Number3)
+        Me.Controls.Add(Me.Number2)
+        Me.Controls.Add(Me.Number1)
+        Me.Controls.Add(Me.TextB1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextB1 As TextBox
+    Friend WithEvents Number1 As Button
+    Friend WithEvents Number2 As Button
+    Friend WithEvents Number3 As Button
+    Friend WithEvents Number4 As Button
+    Friend WithEvents Number5 As Button
+    Friend WithEvents Number6 As Button
+    Friend WithEvents Number7 As Button
+    Friend WithEvents Number8 As Button
+    Friend WithEvents Number9 As Button
+    Friend WithEvents OperationClear As Button
+    Friend WithEvents OperationPlus As Button
+    Friend WithEvents OperationMinus As Button
+    Friend WithEvents OperationEqual As Button
+    Friend WithEvents OperationDivide As Button
+    Friend WithEvents OperationMultiply As Button
+End Class
---
+++ Calculator/Form1.vb
@@ -0,0 +1,80 @@
+﻿Public Class Form1
+    Dim Result As Integer
+    Dim button As Button
+    Dim Calc As Double
+
+    Private Sub ClickMethod(sender As Object, e As EventArgs) Handles Number1.Click, Number2.Click, Number3.Click, Number4.Click, Number5.Click, Number6.Click, Number7.Click, Number8.Click, Number9.Click, OperationPlus.Click, OperationMinus.Click, OperationMultiply.Click, OperationDivide.Click, OperationEqual.Click, OperationClear.Click
+        button = CType(sender, Button)
+        If button.Name = "Number1" Then
+            TextB1.Text = TextB1.Text + Number1.Text
+        End If
+
+        If button.Name = "Number2" Then
+            TextB1.Text = TextB1.Text + Number2.Text
+        End If
+
+        If button.Name = "Number3" Then
+            TextB1.Text = TextB1.Text + Number3.Text
+
+        End If
+
+        If button.Name = "Number4" Then
+            TextB1.Text = TextB1.Text + Number4.Text
+        End If
+
+        If button.Name = "Number5" Then
+            TextB1.Text = TextB1.Text + Number5.Text
+        End If
+
+        If button.Name = "Number6" Then
+            TextB1.Text = TextB1.Text + Number6.Text
+        End If
+
+        If button.Name = "Number7" Then
+            TextB1.Text = TextB1.Text + Number7.Text
+        End If
+
+        If button.Name = "Number8" Then
+            TextB1.Text = TextB1.Text + Number8.Text
+        End If
+
+        If button.Name = "Number9" Then
+            TextB1.Text = TextB1.Text + Number9.Text
+        End If
+
+        If button.Name = "OperationPlus" Then
+            Calc = TextB1.Text + "+"
+
+            TextB1.Text = ""
+        End If
+
+        If button.Name = "OperationMinus" Then
+            Calc = TextB1.Text
+            TextB1.Text = ""
+            TextB1.Text = TextB1.Text + "-"
+        End If
+
+        If button.Name = "OperationMultiply" Then
+            Calc = TextB1.Text
+            TextB1.Text = ""
+            TextB1.Text = TextB1.Text + "*"
+        End If
+
+        If button.Name = "OperationDivide" Then
+            Calc = TextB1.Text
+            TextB1.Text = ""
+            TextB1.Text = TextB1.Text + "/"
+        End If
+
+        If button.Name = "OperationEqual" Then
+            Calc += TextB1.Text
+            Result = New DataTable().Compute(Calc, Nothing)
+            TextB1.Text = Result
+        End If
+
+        If button.Name = "OperationClear" Then
+            TextB1.Text = ""
+        End If
+
+    End Sub
+End Class
